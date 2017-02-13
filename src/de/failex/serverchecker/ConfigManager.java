f2627753b47ce36cc2d4c25804702bf4b521685b
Felix Naumann <felix@emilnaumann.de>
2017-02-13 10:06:55 +0100

    added comments

--- src/de/failex/serverchecker/ConfigManager.java
+++ src/de/failex/serverchecker/ConfigManager.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+/*
+* Yes i was used to YAML Configs from bukkit, so much so that i actually
+* used the same Classes...
+*/
 public class ConfigManager {
 	public File config = new File("tm2config.yml");
 	public FileConfiguration con = YamlConfiguration.loadConfiguration(config);
