e6ccb0181aac7d1875d3b801a2c9de33c4f413e4
Felix Naumann <felix@emilnaumann.de>
2017-02-13 10:08:49 +0100

    added comments

--- src/de/failex/serverchecker/SendMessage.java
+++ src/de/failex/serverchecker/SendMessage.java
@@ -2,6 +2,9 @@ package de.failex.serverchecker;
 
 import javax.swing.JOptionPane;
 
+/*
+* Don't ask why i made this class in the first place because i don't know..
+*/
 public class SendMessage {
 
 	public SendMessage(String msg, String title, int style) {
